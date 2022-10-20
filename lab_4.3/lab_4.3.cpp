b78cb99519ea4e6b485db4c3f41246c07629894a
viksi01 <victoria.pyatnicka@gmail.com>
2022-10-21 00:32:19 +0300

    master_commit1

---
+++ lab_4.3/lab_4.3.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, xp, xk, dx, a, b, c, F ;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "a = "; cin >> a;
+	cout << "b = "; cin >> b;
+	cout << "c = "; cin >> c;
+
+
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(5) << "x" << "     |"
+		<< setw(7) << "F" << "       |" << endl;
+	cout << "---------------------------" << endl;
+
+	x = xp;
+	while (x <= xk)
+	{
+		if (x < 5 && b != 0)
+			F = a * pow((x + 7), 2) - b;
+		else
+			if (x > 5 && b == 0)
+				F = (x - c * a) / (a * x);
+			else
+				F = x / c;
+		
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< "   |" << setw(10) << setprecision(3) << F
+			<< "    |" << endl;
+		x += dx;
+	}
+	cout << "---------------------------" << endl;
+	return 0;
+}
+
